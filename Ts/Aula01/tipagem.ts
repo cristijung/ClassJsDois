103ebd93ac46c8c77d04dea4e6a53192d94cebb1
Cristina Jung <cristijung@gmail.com>
2024-08-19 15:32:47 -0300

    add interface e type

--- Ts/Aula01/tipagem.ts
+++ Ts/Aula01/tipagem.ts
@@ -42,6 +42,55 @@ let meuCarro: Carro = {
     ano: 2014,    
 };
 
+//exemplo de interface estendida
+//interface base para representar um animal
+interface Animal {
+    nome: string;
+    especie: string;
+    som(): void;
+}
+//interface estendida para representar o cachorro
+interface Cachorro extends Animal {
+    raca: string;
+    abanarRabo(): void;
+}
+
+//criando o objeto
+let meuCachorro: Cachorro = {
+    nome: 'Otto',
+    especie: 'canino',
+    raca: 'srd',
+    som: function() {
+        console.log('au, au ...');
+    },
+    abanarRabo: function() {
+        console.log('abanando o rabinho feliz da vida!!')
+    }
+};
+
+meuCachorro.som();
+meuCachorro.abanarRabo();
+
+//Type
+type Pessoa = {
+    nome: string;
+    idade: number;
+    ativo: boolean;
+};
+
+//Type com tupla
+//lat e long
+type Coordenadas = [number, number]; 
+
+//Type com união
+type Status = 'ativo' | 'inativo' | 'pendente';
+
+//Type com interseção
+type PessoaComEndereco = Pessoa & {
+    endereco: string;
+};
+
+
 //Literais
 //Tem função de restringir o valore de uma variável e um conjunto específico de valores literais
 let diracao: 'cima' | 'baixo' | 'esquerda' | 'direita' = 'cima';
